35a8ea71e93feb882509981b73ab3ffa6ccc3ec2
Saurabh Gupta <saurabhg1500@gmail.com>
2022-07-04 13:59:37 +0530

    Task 3 Done

--- Api/controller/etherController.js
+++ Api/controller/etherController.js
@@ -5,55 +5,31 @@ const etherService = require('../services/etherService')
 // fetch crypto Transaction controller
 
 router.get('/transactions', async(req, res, next)  => {
-    
     const [error,errorMsg, result] = await etherService.fetchCrptoTransaction(req);
-
     if (error) {
-        return res.status(500).json({
-            message: errorMsg
-        })
+        return res.status(500).json({message: errorMsg})
     } else {
-        res.status(200).json({
-            response: result
-        
-        })
+        res.status(200).json({response: result})
     }
 });
 
-
 // fetch current balance and current price controller
-
 router.get('/getbalance', async(req, res, next)  => {
-    
     const [error,errorMsg, result] = await etherService.getCurrentBalance(req);
-
     if (error) {
-        return res.status(500).json({
-            message: errorMsg
-        })
+        return res.status(500).json({message: errorMsg})
     } else {
-        res.status(200).json({
-            response: result
-        
-        })
+        res.status(200).json({response: result})
     }
 });
 
 // calculate current balance 
-
 router.get('/calculatebalance', async(req, res, next)  => {
-    
     const [error,errorMsg, result] = await etherService.calculateCurrentBalance(req);
-
     if (error) {
-        return res.status(500).json({
-            message: errorMsg
-        })
+        return res.status(500).json({message: errorMsg})
     } else {
-        res.status(200).json({
-            response: result
-        
-        })
+        res.status(200).json({response: result})
     }
 });
 
